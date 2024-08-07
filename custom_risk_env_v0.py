00b27d25e2dce53c75fccfe529cdfcea444a2b1c
Max <32302710+MX-2000@users.noreply.github.com>
2024-08-07 08:46:45 +0200

    Implement observation and action space
    Observation space doen't use graph representation for now

--- custom_risk_env_v0.py
+++ custom_risk_env_v0.py
@@ -4,6 +4,7 @@ import gymnasium as gym
 from gymnasium import spaces
 
 from game.game import Game
+from game.player import Player, Player_Random
 
 
 class RiskEnv_Choice_is_attack_territory(gym.Env):
@@ -12,7 +13,33 @@ class RiskEnv_Choice_is_attack_territory(gym.Env):
     def __init__(self, game: Game, render_mode=None) -> None:
         self.game = game
 
-        self.observation_space = spaces.Dict()
+        num_territories = len(game.game_map.territories)
+        max_troops = 10e4
+        num_players = game.player_nb
+
+        self.observation_space = spaces.Dict(
+            {
+                "territory_ids": spaces.MultiDiscrete(
+                    [num_territories for _ in range(num_territories)]
+                ),  # [1,....n] which can take values from 0 to n
+                "num_troops": spaces.MultiDiscrete(
+                    [max_troops for _ in range(num_territories)]
+                ),  # Vector (n,) for number of troops
+                "player_ids": spaces.MultiDiscrete(
+                    [num_players for _ in range(num_territories)]
+                ),  # Vector (n,) for the assigned player id
+                "connections": spaces.MultiBinary(
+                    num_territories * num_territories
+                ),  # Flattened adjacency matrix
+            }
+        )
 
         # We can only chose which territory to attack
         self.action_space = spaces.Discrete(len(self.game.game_map.territories))
+
+
+if __name__ == "__main__":
+    p1 = Player_Random("p1")
+    p2 = Player_Random("p2")
+    game = Game("test_map_v0", [p1, p2])
+    env = RiskEnv_Choice_is_attack_territory(game, render_mode=None)
